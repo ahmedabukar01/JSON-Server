fad158e41d8fa46a2e3554cee480e2932e210ff1
ahmed <ahmedlearn01@gmail.com>
2021-06-22 10:45:13 -0700

    end point

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.blogs');
+
+const renderPosts = async ()=>{
+    const uri = 'http://localhost:3000/posts';
+    const res = await fetch(uri);
+    const posts = await res.json();
+
+    let templete = '';
+    posts.forEach(post=>{
+        templete += `
+            <div class="post">
+                <h2>${post.title}</h2>
+                <p><small>${post.likes} Likes </small></p>
+                <p>${post.body.slice(0,200)}</p>
+                <a href="/detail.html?${post.id}">Read more</a>
+            </div>
+        `;
+    })
+
+    container.innerHTML = templete;
+
+}
+window.addEventListener('DOMContentLoaded',(e)=>renderPosts());
